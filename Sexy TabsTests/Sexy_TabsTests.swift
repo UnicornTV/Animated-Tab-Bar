47fde68dfe28ae55207f5434bb3e7beaced6f284
Clay McIlrath <clay.mcilrath@gmail.com>
2014-12-19 04:45:24 -0500

    Initial push

---
+++ Sexy TabsTests/Sexy_TabsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sexy_TabsTests.swift
+//  Sexy TabsTests
+//
+//  Created by Clayton McIlrath on 12/19/14.
+//  Copyright (c) 2014 SwiftCast.TV. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sexy_TabsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
